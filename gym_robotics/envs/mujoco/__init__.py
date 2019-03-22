f7d1c2163644a7e1fbd25cddf25747dd38b8224b
Jon Michaux <jmichaux@ttic.edu>
2019-03-22 13:40:37 -0700

    adding mujoco fetch stuff

---
+++ gym_robotics/envs/mujoco/__init__.py
@@ -0,0 +1,11 @@
+# Standard Fetch Environments
+from gym_robotics.envs.mujoco.fetch.fetch_env import FetchEnv
+from gym_robotics.envs.mujoco.fetch.standard.reach import FetchReachEnv
+from gym_robotics.envs.mujoco.fetch.standard.push import FetchPushEnv
+from gym_robotics.envs.mujoco.fetch.standard.slide import FetchSlideEnv
+from gym_robotics.envs.mujoco.fetch.standard.pick_and_place import FetchPickAndPlaceEnv
+
+# Multi-task Fetch Environments
+
+
+# Residual Fetch Environments
